ddb6fd8790f0448ad4305419eb8d560d238c1ae8
Sudheer <info.sudheer30@gmail.com>
2023-08-21 12:03:16 +0530

    code push

--- variables.sh
+++ variables.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 
-PERSON1=Sudheer
-PERSON2=ANU
+PERSON1=$1 # this $1 is a runtime variable we should pass 
+PERSON2=$2 # this $2 is a second runtime variable we should pass
 
 
 echo $PERSON1 hi $PERSON2
