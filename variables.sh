3e9d1ae8e392e9bc9c7df543dd7992d7cdc2a8df
Sudheer <info.sudheer30@gmail.com>
2023-08-21 11:59:59 +0530

    code push

--- variables.sh
+++ variables.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 
-PERSON1= Sudheer
-PERSON2= ANU
+PERSON1=Sudheer
+PERSON2=ANU
 
 
 echo $PERSON1 hi $PERSON2
-echo $PERSON2 how are you $PERSON2 good
+echo $PERSON2 how are you $PERSON1 good
