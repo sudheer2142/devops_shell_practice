b75640e1cf932ae697324b8d599065a2eed0deb0
Sudheer <info.sudheer30@gmail.com>
2023-08-21 12:11:21 +0530

    code push

---
+++ varibles03.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# this is to take input from user and display & also restricting the echo of input with -s
+
+
+echo " Hi please enter username:"
+
+read "USERNAME"
+
+echo " $USERNAME hi "
