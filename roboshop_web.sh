45fed6dbe9e18e6b985b9b58a40b4ba7aeaa3df5
Sudheer <info.sudheer30@gmail.com>
2023-08-22 16:02:39 +0530

    code push

--- roboshop_web.sh
+++ roboshop_web.sh
@@ -18,15 +18,24 @@ then
 fi
 
 VALIDATE(){
-    if[ $1 -ne 0 ];
-        then 
-            echo -e "$2...$R Failure $N "
-            exit 1
-        else
-             echo -e "$2...$G SUCCESS $N "
+    if [ $1 -ne 0 ];
+    then 
+        echo -e "$2...$R Failure $N "
+        exit 1
+    else
+         echo -e "$2...$G SUCCESS $N "
          
     fi
 
+}
+VALIDATE2(){
+    if [ $1 -ne 0 ];
+    then
+        echo -e "$2 ... $R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
 }
 yum install nginx -y &>> LOG_FILE
 
