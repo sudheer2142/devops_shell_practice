a8bb51e829c07c7c05847e6d1d34da4e13058dc5
Sudheer <info.sudheer30@gmail.com>
2023-08-22 15:54:32 +0530

    code push

---
+++ roboshop_web.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+LOGFILE_DIRECTORY=/tmp
+DATE=$(date +%F:%H:%M:%S)
+SCRIPT_NAME=$0
+LOGFILE=$LOGFILE_DIRECTORY/$SCRIPT_NAME-$DATE.log
+USERID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
+if [ USERID -ne 0 ]
+then 
+ echo -e "$R ERROR: Hai run the script with root user $N"
+ exit 1
+fi
+
+VALIDATE (){
+
+    if[ $1 -ne 0 ]
+
+        then 
+        echo -e "$2...$R Failure $N "
+        exit 1
+        else
+         echo -e "$2...$G SUCCESS $N "
+         
+    fi
+
+}
+yum install nginx -y &>> LOG_FILE
+
+VALIDATE $? " nginx installation "
+
+systemctl enable nginx &>> LOG_FILE
+
+VALIDATE $? " nginx enable "
+
+systemctl start nginx &>> LOG_FILE
+
+VALIDATE $? " nginx start"
+rm -rf /usr/share/nginx/html/* &>> LOG_FILE
+
+VALIDATE $? " REmoving the default nginx html code"
+
+curl -o /tmp/web.zip https://roboshop-builds.s3.amazonaws.com/web.zip &>> LOG_FILE
+
+VALIDATE $? "Downloading the souce code"
+
+cd /usr/share/nginx/html &>> LOG_FILE
+
+VALIDATE $? "Moved to the html path"
+
+unzip /tmp/web.zip &>> LOG_FILE
+
+VALIDATE $? " unzipped the code in tmp folder"
+
+
+cp nginx.conf /etc/nginx/default.d/roboshop.conf  &>> LOG_FILE
+
+VALIDATE $? " editting the conf folder"
+
+systemctl restart nginx &>> LOG_FILE
+
+VALIDATE $? " RESTART Triggered"
+
+
+
