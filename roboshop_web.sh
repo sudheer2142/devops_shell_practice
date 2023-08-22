5d2645d3e318a098bb8d4745f1d04e07d8283a0a
Sudheer <info.sudheer30@gmail.com>
2023-08-22 15:58:51 +0530

    code push

--- roboshop_web.sh
+++ roboshop_web.sh
@@ -11,16 +11,16 @@ G="\e[32m"
 N="\e[0m"
 Y="\e[33m"
 
-if [ USERID -ne 0 ]
+if [ USERID -ne 0 ];
 then 
  echo -e "$R ERROR: Hai run the script with root user $N"
  exit 1
+
 fi
 
 VALIDATE (){
 
-    if[ $1 -ne 0 ]
-
+    if[ $1 -ne 0 ];
         then 
         echo -e "$2...$R Failure $N "
         exit 1
